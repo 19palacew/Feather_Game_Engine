a0f5d9b2eaea5bbfa4401bdf40996b5c5d0ca6e9
19palacew <67712126+19palacew@users.noreply.github.com>
2022-12-30 09:04:42 -0800

    Checkpoint

--- draw-scene.js
+++ draw-scene.js
@@ -7,6 +7,7 @@ function drawScene(gl, programInfo, gameObjects) {
     gl.clearColor(1, 0.0, 0.0, 1.0); // Clear to black, fully opaque
     gl.clearDepth(1.0); // Clear everything
     gl.enable(gl.DEPTH_TEST); // Enable depth testing
+    gl.enable(gl.CULL_FACE); // Enable depth testing
     gl.depthFunc(gl.LEQUAL); // Near things obscure far things
   
     // Clear the canvas before we start drawing on it.
